e5cbd796f5ab63937e4b5803612211df27af9c61
Art Tsai <arttsai@gmail.com>
2025-04-11 21:01:57 +0800

    app.vue - counter

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,29 @@
-<script setup>
-</script>
-
 <template>
   <h1>Hello Vue!</h1>
+  <h2>Counter: {{ counter }}</h2>
+  <button @click="increment">Increment</button>
+  <button @click="decrement">Decrement</button>
+  <button @click="reset">Reset</button>
+
+  <h2>Message: {{ counterMessage }}</h2>
 </template>
+
+<script setup>
+import { ref, computed } from 'vue';
+
+const counter = ref(0);
+const increment = () => {
+  counter.value++;
+};
+const decrement = () => {
+  counter.value--;
+};
+const reset = () => {
+  counter.value = 0;
+};
+
+const counterMessage = computed(() => {
+  return 'The counter is ' + counter.value;
+});
+
+</script>
