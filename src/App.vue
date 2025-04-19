efe648b34bcb244ce0567024458a810e25cf3a43
Art Tsai <arttsai@gmail.com>
2025-04-19 15:24:53 +0800

    讓 List 有自動排序的響應能力

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container p-4">
     <h1 class="title is-4 has-text-centered">Hello Vue!</h1>
-    <div v-for="item in voteItems" class="p-4">
+    <div v-for="item in sortedItems" class="p-4">
       <VoteItem :item="item" />
     </div>
   </div>
@@ -9,9 +9,12 @@
 
 <script setup>
 import VoteItem from './components/VoteItem.vue';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import voteList from './data/votes.js'
 voteList.sort((a, b) => b.votes - a.votes)
 const voteItems = ref(voteList)
+const sortedItems = computed(() => {
+  return voteItems.value.sort((a, b) => b.votes - a.votes);
+});
 
 </script>
