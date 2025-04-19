48194e3cc2bce545694ecb70a6e8c1ab7212ea52
Art Tsai <arttsai@gmail.com>
2025-04-19 15:26:48 +0800

    使用 v-if, v-else

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="container p-4">
     <h1 class="title is-4 has-text-centered">Hello Vue!</h1>
+    <div v-if="maxVotes > 20" class="has-text-centered">
+      Wow! The max votes has succeeded 20!!
+    </div>
+    <div v-else class="has-text-centered">
+      Please Vote!
+    </div>
     <div v-for="item in sortedItems" class="p-4">
       <VoteItem :item="item" />
     </div>
@@ -15,6 +21,12 @@ voteList.sort((a, b) => b.votes - a.votes)
 const voteItems = ref(voteList)
 const sortedItems = computed(() => {
   return voteItems.value.sort((a, b) => b.votes - a.votes);
+});
+
+const maxVotes = computed(() => {
+  return voteItems.value.reduce((max, item) => {
+    return Math.max(max, item.votes);
+  }, 0);
 });
 
 </script>
