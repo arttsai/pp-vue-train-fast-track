e3bfbad35a25eebb5cd888634f6c094ba7223633
Art Tsai <arttsai@gmail.com>
2025-04-22 13:06:19 +0800

    加上瀏覽器上網頁頁籤名稱

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,33 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
+import { meta } from 'eslint-plugin-vue'
 
 const routes = [
-  { path: '/', component: HomeView },
-  { path: '/about', component: AboutView },
-  { path: '/upvote', component: () => import('../views/UpVoteView.vue') },
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+    meta: {
+      title: '首頁',
+    },
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: AboutView,
+    meta: {
+      title: '關於我們',
+    },
+  },
+  {
+    path: '/upvote',
+    name: 'upvote',
+    component: () => import('../views/UpVoteView.vue'),
+    meta: {
+      title: 'Blog 投票',
+    },
+  },
 ]
 
 const router = createRouter({
