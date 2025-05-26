7b7e46ba2ff683d6ccb2461af534195f05cf4471
Art Tsai <arttsai@gmail.com>
2025-05-26 20:33:33 +0800

    建立 Login 頁面輸入欄位

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,10 +1,94 @@
 <template>
-    <div class="container has-text-centered">
-        <h1 class="title is-3">登入 (Login)</h1>
-        <button class="button is-primary" @click="login">
-            點按我，就登入
-        </button>
+    <div>
+        <h1 class="subtitle is-3 has-text-centered">Login</h1>
+
+        <div class="container columns">
+            <div class="column"></div>
+
+            <form class="column is-half" @submit.prevent="login">
+                <div class="field">
+                    <label class="label">Username</label>
+                    <div class="control has-icons-left has-icons-right">
+                        <input class="input is-success" type="text" placeholder="Text input">
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-user"></i>
+                        </span>
+                        <span class="icon is-small is-right">
+                            <i class="fas fa-check"></i>
+                        </span>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label for="" class="label">密碼</label>
+                    <div class="control">
+                        <input type="password" class="input" placeholder="密碼">
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label">Email</label>
+                    <div class="control has-icons-left has-icons-right">
+                        <input class="input is-danger" type="email" placeholder="Email input" value="hello@">
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-envelope"></i>
+                        </span>
+                        <span class="icon is-small is-right">
+                            <i class="fas fa-exclamation-triangle"></i>
+                        </span>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label">主題選項</label>
+                    <div class="control">
+                        <div class="select">
+                            <select>
+                                <option>選項一</option>
+                                <option>選項二</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <div class="control">
+                        <label class="checkbox">
+                            <input type="checkbox">
+                            我同意條款
+                        </label>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <div class="control ">
+                        <label class="radio mr-3">
+                            <input type="radio" name="question">
+                            Yes
+                        </label>
+                        <label class="radio">
+                            <input type="radio" name="question">
+                            No
+                        </label>
+                    </div>
+                </div>
+
+                <div class="field is-grouped">
+                    <div class="control">
+                        <button class="button is-link" type="submit">登入</button>
+                    </div>
+                    <div class="control">
+                        <button class="button is-link is-light" @click="cancel">取消</button>
+                    </div>
+                </div>
+            </form>
+
+            <div class="column"></div>
+        </div>
     </div>
+
+
+
 </template>
 
 <script setup>
