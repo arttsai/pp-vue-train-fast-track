9a66108228f068b769762f8e641c5fb2e6b6757f
Art Tsai <arttsai@gmail.com>
2025-05-27 11:14:45 +0800

    挷定欄位, 建立表單驗証

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -7,9 +7,10 @@
 
             <form class="column is-half" @submit.prevent="login">
                 <div class="field">
-                    <label class="label">Username</label>
+                    <label for="username" class="label">使用者帳號</label>
                     <div class="control has-icons-left has-icons-right">
-                        <input class="input is-success" type="text" placeholder="Text input">
+                        <input class="input is-success" type="text" placeholder="Text input" id="username"
+                            name="username" v-model="formData.username">
                         <span class="icon is-small is-left">
                             <i class="fas fa-user"></i>
                         </span>
@@ -17,19 +18,23 @@
                             <i class="fas fa-check"></i>
                         </span>
                     </div>
+                    <p v-if="formErrors.username" class="help is-danger">{{ formErrors.username }}</p>
                 </div>
 
                 <div class="field">
-                    <label for="" class="label">密碼</label>
+                    <label for="password" class="label">密碼</label>
                     <div class="control">
-                        <input type="password" class="input" placeholder="密碼">
+                        <input type="password" class="input" placeholder="密碼" id="password" name="password"
+                            v-model="formData.password">
                     </div>
+                    <p v-if="formErrors.password" class="help is-danger">{{ formErrors.password }}</p>
                 </div>
 
                 <div class="field">
-                    <label class="label">Email</label>
+                    <label for="email" class="label">Email</label>
                     <div class="control has-icons-left has-icons-right">
-                        <input class="input is-danger" type="email" placeholder="Email input" value="hello@">
+                        <input class="input is-danger" type="email" placeholder="Email input" id="email" name="email"
+                            v-model="formData.email">
                         <span class="icon is-small is-left">
                             <i class="fas fa-envelope"></i>
                         </span>
@@ -37,40 +42,45 @@
                             <i class="fas fa-exclamation-triangle"></i>
                         </span>
                     </div>
+                    <p v-if="formErrors.email" class="help is-danger">{{ formErrors.email }}</p>
                 </div>
 
                 <div class="field">
-                    <label class="label">主題選項</label>
+                    <label for="subject" class="label">主題選項</label>
                     <div class="control">
                         <div class="select">
-                            <select>
-                                <option>選項一</option>
-                                <option>選項二</option>
+                            <select id="subject" name="subject" v-model="formData.subject">
+                                <option disabled value="">請選擇</option>
+                                <option value="1">選項一</option>
+                                <option value="2">選項二</option>
                             </select>
                         </div>
                     </div>
+                    <p v-if="formErrors.subject" class="help is-danger">{{ formErrors.subject }}</p>
                 </div>
 
                 <div class="field">
                     <div class="control">
                         <label class="checkbox">
-                            <input type="checkbox">
+                            <input type="checkbox" id="agree" name="agree" v-model="formData.agree">
                             我同意條款
                         </label>
                     </div>
+                    <p v-if="formErrors.agree" class="help is-danger">{{ formErrors.agree }}</p>
                 </div>
 
                 <div class="field">
                     <div class="control ">
                         <label class="radio mr-3">
-                            <input type="radio" name="question">
+                            <input type="radio" name="question" v-model="formData.question" value="yes">
                             Yes
                         </label>
                         <label class="radio">
-                            <input type="radio" name="question">
+                            <input type="radio" name="question" v-model="formData.question" value="no">
                             No
                         </label>
                     </div>
+                    <p v-if="formErrors.question" class="help is-danger">{{ formErrors.question }}</p>
                 </div>
 
                 <div class="field is-grouped">
@@ -86,27 +96,63 @@
             <div class="column"></div>
         </div>
     </div>
-
-
-
 </template>
 
 <script setup>
 import { useUserStore } from '@/stores/user.js'
 import { useRoute, useRouter } from 'vue-router'
+import { ref } from 'vue'
 
 const userStore = useUserStore()
 const route = useRoute()
 const router = useRouter()
 
-console.log('route', route)
+const formData = ref({
+    username: '',
+    password: '',
+    email: '',
+    subject: '',
+    agree: false,
+    question: ''
+})
+
+const formErrors = ref({
+    username: '',
+    password: '',
+    email: '',
+    subject: '',
+    agree: '',
+    question: ''
+})
+
+const formValidate = () => {
+    formErrors.value.username = formData.value.username ? '' : '使用者帳號為必填'
+    formErrors.value.password = formData.value.password ? '' : '密碼為必填'
+
+    if (!formData.value.email) {
+        formErrors.value.email = '請輸入 Email';
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.value.email)) {
+        formErrors.value.email = 'Email 格式不正確';
+    } else {
+        formErrors.value.email = '';
+    }
+
+    formErrors.value.subject = formData.value.subject ? '' : '主題選項為必填'
+    formErrors.value.agree = formData.value.agree ? '' : '您必須同意條款'
+    formErrors.value.question = formData.value.question ? '' : '請回答問題'
+
+    return Object.values(formErrors.value).every(error => !error)  // 傳回是否有錯誤
+}
 
 const login = () => {
+    if (!formValidate()) {
+        window.alert('表單驗證失敗，請檢查輸入的資料')
+        return
+    }
+
     userStore.login(1)
 
-    console.log('userId', userStore.userId)
-    console.log('userName', userStore.userName)
-    console.log('isLogin', userStore.isLogin)
+    console.log('Form Data:', formData.value)
 
     if (route.query.redirect) {
         router.push(route.query.redirect)
