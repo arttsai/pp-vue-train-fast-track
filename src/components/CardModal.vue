29cdac74bf513b8c3515a22956f25dd488adf6d1
Art Tsai <arttsai@gmail.com>
2025-06-13 14:43:02 +0800

    使用 defineModel()

--- src/components/CardModal.vue
+++ src/components/CardModal.vue
@@ -1,22 +1,31 @@
 <template>
     <div>
-        <div class="modal">
+        <div class="modal" :class="{ 'is-active': showModal }">
             <div class="modal-background"></div>
             <div class="modal-card">
                 <header class="modal-card-head">
                     <p class="modal-card-title">Modal title</p>
-                    <button class="delete" aria-label="close"></button>
                 </header>
                 <section class="modal-card-body">
                     <!-- Content ... -->
                 </section>
                 <footer class="modal-card-foot">
                     <div class="buttons">
-                        <button class="button is-success">Save changes</button>
-                        <button class="button">Cancel</button>
+                        <button class="button is-success" @click="showModal = false">Save changes</button>
+                        <button class="button" @click="showModal = false">Cancel</button>
                     </div>
                 </footer>
             </div>
         </div>
     </div>
 </template>
+
+<script setup>
+const showModal = defineModel({
+    type: Boolean,
+    default: false,
+    required: true
+});
+
+
+</script>
