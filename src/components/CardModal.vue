741d8f23851308c7a27a52ed2242149811fb7a25
Art Tsai <arttsai@gmail.com>
2025-06-13 16:12:26 +0800

    對話窗 slot, 資料輸入與檢查

--- src/components/CardModal.vue
+++ src/components/CardModal.vue
@@ -4,16 +4,24 @@
             <div class="modal-background"></div>
             <div class="modal-card">
                 <header class="modal-card-head">
-                    <p class="modal-card-title">Modal title</p>
+                    <p class="modal-card-title">
+                        <slot name="header">
+                            <span style="color: red; font-weight: bold; font-size: 1.1em;">
+                                請父元件提供 header slot
+                            </span>
+                        </slot>
+                    </p>
                 </header>
                 <section class="modal-card-body">
-                    <!-- Content ... -->
+                    <slot>請父元件提供預設 slot 的內容</slot>
                 </section>
                 <footer class="modal-card-foot">
-                    <div class="buttons">
-                        <button class="button is-success" @click="showModal = false">Save changes</button>
-                        <button class="button" @click="showModal = false">Cancel</button>
-                    </div>
+                    <slot name="footer">
+                        <div class="buttons">
+                            <button class="button is-success" @click="save">存檔</button>
+                            <button class="button" @click="showModal = false">放棄</button>
+                        </div>
+                    </slot>
                 </footer>
             </div>
         </div>
@@ -26,6 +34,5 @@ const showModal = defineModel({
     default: false,
     required: true
 });
-
 
 </script>
--- src/views/ModalView.vue
+++ src/views/ModalView.vue
@@ -2,8 +2,33 @@
     <div>
         <div class="container is-max-desktop">
             <h1 class="title is-4 has-text-centered">Modal Demo (對話窗)</h1>
-            <button class="button is-primary" @click="showModal = true">Launch Card Modal</button>
-            <CardModal v-model="showModal"></CardModal>
+            <button class="button is-primary" @click="showDialog">Launch Card Modal</button>
+            <CardModal v-model="showModal">
+                <template #header>
+                    請提供基本資料
+                </template>
+                <template #footer>
+                    <div class="buttons ml-auto">
+                        <button class="button is-primary" @click="save">存檔</button>
+                        <button class="button is-link" @click="cancel">放棄</button>
+                    </div>
+                </template>
+
+                <div class="field">
+                    <label class="label">姓名</label>
+                    <div class="control">
+                        <input id="name" v-model="inquery.name" class="input" type="text" placeholder="Text input">
+                    </div>
+                    <span class="help is-danger" v-if="error.name">{{ error.name }}</span>
+                </div>
+                <div class="field">
+                    <label class="label">年紀</label>
+                    <div class="control">
+                        <input id="age" v-model="inquery.age" class="input" type="text" placeholder="Text input">
+                    </div>
+                    <span class="help is-danger" v-if="error.age">{{ error.age }}</span>
+                </div>
+            </CardModal>
         </div>
     </div>
 </template>
@@ -14,6 +39,60 @@ import CardModal from '@/components/CardModal.vue';
 
 const showModal = ref(false);
 
-// 程式還沒有寫完，不能執行 
+const inquery = ref({
+    name: '',
+    age: ''
+});
+
+const error = ref({
+    name: '',
+    age: ''
+});
+
+const showDialog = () => {
+    // Reset input values and errors
+    inquery.value.name = '';
+    inquery.value.age = '';
+    error.value.name = '';
+    error.value.age = '';
+
+    showModal.value = true;
+};
+
+const save = () => {
+    if (!validate()) return;
+    showModal.value = false;
+};
+
+const validate = () => {
+    // reset error messages
+    error.value.name = '';
+    error.value.age = '';
+
+    if (!inquery.value.name) {
+        error.value.name = '請輸入姓名';
+    }
+
+    const age = Number(inquery.value.age);
+    if (!inquery.value.age || isNaN(age) || age <= 15) {
+        error.value.age = '請輸入年齡，且必須大於 15 歲';
+    }
+
+    if (error.value.name || error.value.age) {
+        return false;
+    }
+
+    return true;
+};
+
+const cancel = () => {
+    // Reset input values and errors
+    inquery.value.name = '';
+    inquery.value.age = '';
+    error.value.name = '';
+    error.value.age = '';
+
+    showModal.value = false;
+};
 
 </script>
